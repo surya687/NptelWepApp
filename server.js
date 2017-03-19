ed77e5dc2f4d986b61fa41786792d5ccbb34a138
Surya Kothapalli <suryavenkat53@gmail.com>
2017-03-19 17:27:09 +0530

    Initial commit of NptelWebApp

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+app.get("/",function(req,res){
+  //res.send("Hello World");
+  res.sendFile(path.join(__dirname,'ui','profile.html'));
+});
+app.get("/ui/style.css",function(req,res){
+  res.sendFile(path.join(__dirname,'ui','style.css'));
+})
+app.get("/ui/myPhoto.jpg",function(req,res){
+  res.sendFile(path.join(__dirname,'ui','myPhoto.jpg'));
+})
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
